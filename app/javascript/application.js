2285695d1822afdfaaebd3ba171a3979c0fad27c
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-07-09 11:36:07 -0400

    Rails app spin up

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,3 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
-import "controllers"
+import "./controllers"
