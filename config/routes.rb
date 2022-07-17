dc9050bff136573de013bc57ddcaf0e765009e2f
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-07-17 14:13:49 -0400

    Restart

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
