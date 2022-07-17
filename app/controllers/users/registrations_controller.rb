6eed7dae3efbf679da6716958bcde26eccd60aaf
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-07-17 11:49:13 -0400

    registration controller set up

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,22 @@
+# app/controllers/users/registrations_controller.rb
+class Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  private
+  def resond_with(resource, _opts = {})
+    register_success && return if resource.persisted?
+
+    register_failed
+  end
+
+  def register_success
+    render json: {
+      message: 'Signed up successfully.',
+      user: current_user
+    }, status: :ok
+  end
+
+  def register_failed
+    render json: { message: 'Something went wrong' }, status: :unprocessable_entity
+  end
+end
