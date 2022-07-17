66090111cfca90403658d1ce849deffdbdbf9647
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-07-17 16:11:35 -0400

    Reset AGAIN

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
