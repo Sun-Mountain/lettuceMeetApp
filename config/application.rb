1d734585551004d578e59faf05b065bffae22028
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-07-17 17:35:45 -0400

    initalized backend

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,14 @@ module LettuceMeetApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # This also configures session_options for use below
+    config.session_store :cookie_store, key: '_interslice_session'
+
+    # Required for all session management (regardless of session_store)
+    config.middleware.use ActionDispatch::Cookies
+
+    config.middleware.use config.session_store, config.session_options
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
