7752296b26ed238d99acc60fba3235800620d974
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-07-17 11:37:03 -0400

    create and migrate database

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :jwt_authenticatable,
+         jwt_revocation_strategy: JwtDenylist
+        #  :recoverable, :rememberable, :validatable
 end
