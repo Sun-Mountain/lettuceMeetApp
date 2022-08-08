e3dbc42c1ee5b84985a9041f5f7ec1a053e56219
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-08-07 20:17:04 -0400

    rspec user model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, :email, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,25 @@ RSpec.describe User, type: :model do
   describe 'factory' do
     it { is_expected.to be_valid }
   end
+
+  describe '#first_name' do
+    it 'requires a first_name' do
+      subject.first_name = nil
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe '#last_name' do
+    it 'requires a last_name' do
+      subject.last_name = nil
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe '#email' do
+    it 'requires a email' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
