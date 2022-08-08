96171836facd3c250fb17a1202edef68b44690b6
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-08-07 21:04:09 -0400

    devise errors working

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -34,6 +34,7 @@ Devise.setup do |config|
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
+  config.parent_controller = 'Users::DeviseController'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
