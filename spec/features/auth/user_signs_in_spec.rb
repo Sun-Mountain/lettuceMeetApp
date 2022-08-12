4d8c864800a52f8ded6a2a26282fc83683c93121
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-08-12 14:53:37 -0400

    update rspec tests

--- spec/features/auth/user_signs_in_spec.rb
+++ spec/features/auth/user_signs_in_spec.rb
@@ -5,12 +5,14 @@ require 'rails_helper'
 RSpec.feature 'user signs in' do
   let!(:user) { create :user, password: '12345ABCDEfghi!', password_confirmation: '12345ABCDEfghi!' }
 
-  scenario 'when successful' do
-    visit new_user_session_path
-    fill_in :user_email, with: user.email
-    fill_in :user_password, with: '12345ABCDEfghi!'
-    find('[data-test="submit"]').click
+  context 'when successful' do
+    scenario 'user signs in successfully' do
+      visit new_user_session_path
+      fill_in :user_email, with: user.email
+      fill_in :user_password, with: '12345ABCDEfghi!'
+      find('[data-test="submit"]').click
 
-    expect(page).to have_content('Signed in successfully.')
+      expect(page).to have_content('Signed in successfully.')
+    end
   end
 end
