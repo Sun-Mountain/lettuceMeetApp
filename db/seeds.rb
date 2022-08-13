88885abbc60cd918155fe1b0dc6f6e9bf7376785
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-08-13 17:06:09 -0400

    lint fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,7 +8,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-picard = FactoryBot.create(:user, email: 'picard@gmail.com', first_name: 'Jean Luc', last_name: 'Picard', password: '123456', password_confirmation: '123456',  admin: true)
+FactoryBot.create(:user, email: 'picard@gmail.com', first_name: 'Jean Luc', last_name: 'Picard', password: '123456', password_confirmation: '123456', admin: true)
 FactoryBot.create(:user, email: 'riker@gmail.com', first_name: 'William', last_name: 'Riker', password: '123456', password_confirmation: '123456')
 FactoryBot.create(:user, email: 'laforge@gmail.com', first_name: 'Geordi', last_name: 'La Forge', password: '123456', password_confirmation: '123456')
 FactoryBot.create(:user, email: 'yar@gmail.com', first_name: 'Tasha', last_name: 'yar', password: '123456', password_confirmation: '123456')
