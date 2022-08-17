9e452c86c6dc27b075b60dc58330777f41841dc4
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-08-16 20:02:43 -0400

    new event page

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class EventsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
