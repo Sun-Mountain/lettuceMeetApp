51ce83aa6c24aeae8285beb677730011140a5084
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-08-24 15:24:59 -0400

    event create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,5 +11,26 @@ class EventsController < ApplicationController
   end
 
   def create
+    @user = User.find(user_id)
+    @event = @user.events.new(events_params)
+    binding.pry
+
+    if @event.save
+      flash[:notice] = 'Event created.'
+    else
+      flash[:alert] = 'Event could not be created.'
+    end
+
+    redirect_to root_path
+  end
+
+  private
+
+  def events_params
+    params.require(:event).permit(:title, :start_date, :end_date, :description, :user_id)
+  end
+
+  def user_id
+    current_user.id
   end
 end
