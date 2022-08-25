68b3ed094f6ae005822306652368a2cae6a28af5
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-08-25 14:04:55 -0400

    update event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,16 +15,44 @@ class EventsController < ApplicationController
     @event = @user.events.new(events_params)
 
     if @event.save
-      flash[:notice] = 'Event created.'
+      flash[:notice] = 'Event successfully created.'
+      redirect_to event_path(@event.id)
     else
       flash[:alert] = 'Event could not be created.'
+      redirect_to root_path
     end
-
-    redirect_to root_path
   end
 
   def show
-    @event = Event.find(event_id)
+    @event = Event.find(event_id_param)
+  end
+
+  def edit
+    @event = Event.find(event_id_param)
+  end
+
+  def update
+    @event = Event.find(event_id_param)
+
+    if @event.update(events_params)
+      flash[:notice] = 'Event successfully updated.'
+      redirect_to event_path(@event.id)
+    else
+      flash[:alert] = 'Event could not be updated.'
+      redirect_to edit_event_path(@event.id)
+    end
+  end
+
+  def destroy
+    @event = Event.find(event_id_param)
+
+    if @event.destroy
+      flash[:notice] = 'Event successfully deleted.'
+      redirect_to root_path
+    else
+      flash[:alert] = 'Event could not be deleted.'
+      redirect_to event_path(@event.id)
+    end
   end
 
   private
@@ -33,7 +61,7 @@ class EventsController < ApplicationController
     params.require(:event).permit(:title, :start_date, :end_date, :description, :user_id)
   end
 
-  def event_id
+  def event_id_param
     params.require(:id)
   end
 
