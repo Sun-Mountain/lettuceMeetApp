7fd1ac0ab378ae9b7d9a8acd7acadfe98c581fbb
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-08-26 11:18:50 -0400

    update styling

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :event do
-    user { nil }
+    sequence(:title) { |n| "event_#{n}" }
+    sequence(:start_date) { |n| Date.today }
+
+    public { false }
+    all_day { false }
+    description { 'This is a description for the event' }
+    user_id { 1 }
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create :user }
+  subject { create :event, user_id: user.id }
+
+  describe 'factory' do
+    it { is_expected.to be_valid }
+  end
 end
