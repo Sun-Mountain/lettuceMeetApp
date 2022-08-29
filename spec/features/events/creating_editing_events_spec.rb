c67e76224be2ca35887707f52bcffcbcabf8e887
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-08-29 19:13:50 -0400

    update edit tests

--- spec/features/events/creating_editing_events_spec.rb
+++ spec/features/events/creating_editing_events_spec.rb
@@ -13,8 +13,8 @@ RSpec.feature 'user creates events' do
   context 'user creates an event' do
     scenario 'when successfull' do
       title = 'My Event'
-      startTime = Date.today + 2.days
-      endTime = Date.today + 3.days
+      startDate = Date.today + 2.days
+      endDate = Date.today + 3.days
       description = 'This is the description for my event.'
 
       find('[data-test="new_events_link"]').click
@@ -22,8 +22,8 @@ RSpec.feature 'user creates events' do
       expect(page.body).to include('New Event')
 
       fill_in :event_title, with: title
-      fill_in :event_start_date, with: startTime
-      fill_in :event_end_date, with: endTime
+      fill_in :event_start_date, with: startDate
+      fill_in :event_end_date, with: endDate
       fill_in :event_description, with: description
 
       find('[data-test="submit"]').click
@@ -34,7 +34,7 @@ RSpec.feature 'user creates events' do
 
     scenario 'when unsuccessful' do
       title = 'My Event'
-      startTime = Date.today + 2.days
+      startDate = Date.today + 2.days
 
       find('[data-test="new_events_link"]').click
 
@@ -44,13 +44,12 @@ RSpec.feature 'user creates events' do
 
       expect(page.body).to include('Event could not be created.')
 
-      fill_in :event_title, with: title
-      fill_in :event_start_date, with: startTime
+      fill_in :event_title, with: ''
+      fill_in :event_start_date, with: startDate
 
       find('[data-test="submit"]').click
 
-      expect(page.body).to include('Event successfully created.')
-      expect(page.body).to include(title)
+      expect(page.body).to include('Event could not be created.')
     end
   end
 
@@ -59,15 +58,33 @@ RSpec.feature 'user creates events' do
 
     before(:each) do
       visit event_path(event.id)
+      find('[data-test="edit_event"]').click
+      expect(page).to have_selector("input[value='#{event.title}']")
+      expect(page).to have_selector("input[value='#{event.start_date}']")
     end
 
     scenario 'when successful' do
-      expect(page.body).to have_content(event.title)
-      find('[data-test="edit_event"]').click
+      newTitle = 'My New Title'
+      newStartDate = Date.today + 3.days
+      newEndDate = Date.today + 4.days
+      newDescription = 'Boom Bam'
+
+      fill_in :event_title, with: newTitle
+      fill_in :event_start_date, with: newStartDate
+      fill_in :event_end_date, with: newEndDate
+      fill_in :event_description, with: newDescription
+
+      find('[data-test="update"]').click
+
+      expect(page.body).to have_content('Event successfully updated.')
     end
 
     scenario 'when unsuccessful' do
-      expect(page.body).to have_content(event.title)
+      fill_in :event_title, with: ''
+
+      find('[data-test="update"]').click
+
+      expect(page.body).to have_content('Event could not be updated.')
     end
   end
 end
