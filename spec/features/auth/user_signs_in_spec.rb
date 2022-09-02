7c786f85c222f1df748f8778167bd1878783ad2b
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-09-02 16:13:36 -0400

    refactor: tests

--- spec/features/auth/user_signs_in_spec.rb
+++ spec/features/auth/user_signs_in_spec.rb
@@ -5,14 +5,12 @@ require 'rails_helper'
 RSpec.feature 'user signs in' do
   let!(:user) { create :user, password: '12345ABCDEfghi!', password_confirmation: '12345ABCDEfghi!' }
 
-  context 'user signs in successfully' do
-    scenario 'when successful' do
-      visit new_user_session_path
-      fill_in :user_email, with: user.email
-      fill_in :user_password, with: '12345ABCDEfghi!'
-      find('[data-test="submit"]').click
+  scenario 'when successful' do
+    visit new_user_session_path
+    fill_in :user_email, with: user.email
+    fill_in :user_password, with: '12345ABCDEfghi!'
+    find('[data-test="submit"]').click
 
-      expect(page).to have_content('Signed in successfully.')
-    end
+    expect(page).to have_content('Signed in successfully.')
   end
 end
--- spec/features/events/creating_events_spec.rb
+++ spec/features/events/creating_events_spec.rb
@@ -10,40 +10,38 @@ RSpec.feature 'user creates events' do
     visit root_path
   end
 
-  context 'user creates an event' do
-    scenario 'when successfull' do
-      find('[data-test="new_events_link"]').click
+  scenario 'when successfull' do
+    find('[data-test="new_events_link"]').click
 
-      expect(page.body).to include('New Event')
+    expect(page.body).to include('New Event')
 
-      fill_in :event_title, with: 'My Event'
-      fill_in :event_start_date, with: Date.today + 2.days
-      fill_in :event_end_date, with: Date.today + 3.days
-      fill_in :event_description, with: 'This is the description for my event.'
+    fill_in :event_title, with: 'My Event'
+    fill_in :event_start_date, with: Date.today + 2.days
+    fill_in :event_end_date, with: Date.today + 3.days
+    fill_in :event_description, with: 'This is the description for my event.'
 
-      find('[data-test="submit"]').click
+    find('[data-test="submit"]').click
 
-      expect(page.body).to include('Event successfully created.')
-      expect(page.body).to include(title)
-    end
+    expect(page.body).to include('Event successfully created.')
+    expect(page.body).to include(title)
+  end
 
-    scenario 'when unsuccessful' do
-      start_date = Date.today + 2.days
+  scenario 'when unsuccessful' do
+    start_date = Date.today + 2.days
 
-      find('[data-test="new_events_link"]').click
+    find('[data-test="new_events_link"]').click
 
-      expect(page.body).to include('New Event')
+    expect(page.body).to include('New Event')
 
-      find('[data-test="submit"]').click
+    find('[data-test="submit"]').click
 
-      expect(page.body).to include('Event could not be created.')
+    expect(page.body).to include('Event could not be created.')
 
-      fill_in :event_title, with: ''
-      fill_in :event_start_date, with: start_date
+    fill_in :event_title, with: ''
+    fill_in :event_start_date, with: start_date
 
-      find('[data-test="submit"]').click
+    find('[data-test="submit"]').click
 
-      expect(page.body).to include('Event could not be created.')
-    end
+    expect(page.body).to include('Event could not be created.')
   end
 end
