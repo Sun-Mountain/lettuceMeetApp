cfda91278343f512e53afe0fe3a08788f756153b
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-09-04 17:47:39 -0400

    mark current page nav link

--- app/helpers/nav_helper.rb
+++ app/helpers/nav_helper.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 module NavHelper
+  def current_nav_page
+    'current_nav_page'
+  end
+
+  def home?
+    current_page?(root_path)
+  end
+
+  def edit_profile?
+    current_page?(edit_registration_url(User))
+  end
+
   def current_admin_page
     'current_admin_page'
   end
