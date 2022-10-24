c6a8fe454758da9a4d6beb2d59d6b2e90e7dea4a
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-10-23 20:22:08 -0400

    fix lint

--- app/models/event.rb
+++ app/models/event.rb
@@ -11,8 +11,6 @@ class Event < ApplicationRecord
   def end_date_after_start_date
     return if end_date.blank? || start_date.blank?
 
-    if end_date < start_date
-      errors.add(:end_date, "must be after the start date")
-    end
+    errors.add(:end_date, 'must be after the start date') if end_date < start_date
   end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -9,5 +9,5 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin_all_from 'app/javascript/custom', under: 'custom'
 
-pin "stimulus-flatpickr", to: "https://ga.jspm.io/npm:stimulus-flatpickr@3.0.0-0/dist/index.m.js"
-pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/esm/index.js"
+pin 'stimulus-flatpickr', to: 'https://ga.jspm.io/npm:stimulus-flatpickr@3.0.0-0/dist/index.m.js'
+pin 'flatpickr', to: 'https://ga.jspm.io/npm:flatpickr@4.6.13/dist/esm/index.js'
