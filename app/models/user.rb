c90d3b29d00c3c65e2c2fe9800fa67848be98317
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-11-13 11:16:18 -0500

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,8 @@
 class User < ApplicationRecord
   has_many :events, dependent: :destroy
 
-  after_initialize :set_default_role, if: :new_record?
+  after_create :set_default_role
   enum role: %i[enduser beta admin superadmin]
-
-  def set_default_role
-    unless self.role.nil?
-      return
-    end
-    self.role = :enduser
-  end
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -22,5 +15,11 @@ class User < ApplicationRecord
 
   def name
     "#{first_name} #{last_name}"
+  end
+
+  private
+
+  def set_default_role
+    current_user.role = 0
   end
 end
