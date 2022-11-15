c6f47ae6243b1881216e526c21f54b877beeb47e
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-11-15 08:29:44 -0500

    clear

--- app/models/event.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class Event < ApplicationRecord
-  belongs_to :user
-
-  validates :title, :start_date, :user_id, presence: true
-end
--- db/migrate/20220929003145_remove_authorized_from_users.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveAuthorizedFromUsers < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :authorized, :boolean
-    remove_column :users, :admin, :boolean
-  end
-end
