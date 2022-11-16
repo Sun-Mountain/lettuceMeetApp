f987e8a19fc472c8a9c9bed55720d43dfa54c851
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-11-16 10:47:52 -0500

    sign up login page

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,14 +1,11 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import sessionManager from "@/store/modules/session_manager";
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state: {},
+  mutations: {},
+  actions: {},
   modules: {
-  }
-})
+    sessionManager,
+  },
+});
