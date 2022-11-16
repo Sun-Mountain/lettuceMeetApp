c5d3d8c549f5f43ede60e8e97769f9c2ec12f06b
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-11-16 10:14:30 -0500

    Session manager

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,23 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+  <SessionManager />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SessionManager from "./components/SessionManager.vue";
 export default {
-  name: 'App',
-
+  name: "App",
   components: {
-    HelloWorld,
+    SessionManager,
   },
-
-  data: () => ({
-    //
-  }),
-}
+};
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,18 @@ import store from './store'
 
 loadFonts()
 
+// Load JWT from Local Storage on Refresh
+let localAuthToken = localStorage.auth_token;
+let cookieExists = localAuthToken !== 'undefined' && localAuthToken !== null;
+
+if (cookieExists) {
+  const auth_token = localStorage.getItem("auth_token");
+  const authTokenExists = auth_token !== "undefined" && auth_token !== null;
+  if (authTokenExists) {
+    store.dispatch("loginUserWithToken", { auth_token });
+  }
+}
+
 createApp(App).use(store)
   .use(vuetify)
   .mount('#app')
