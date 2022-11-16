e1129fd7a31a72addf72ceb6d0bd3d1ed691e7e8
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-11-16 12:31:26 -0500

    set up nav and main

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,20 @@
 <script setup>
 import {RouterView} from 'vue-router';
 import Navigation from "./components/Navigation.vue";
-import SessionManager from "./components/SessionManager.vue";
 </script>
 
 <template>
-  <Navigation />
-  <RouterView />
+  <div class="container">
+    <header>
+      <Navigation/>
+    </header>
+
+    <main>
+      <RouterView/>
+    </main>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import '@/assets/stylesheets/application.scss';
 </style>
--- frontend/src/components/Navigation.vue
+++ frontend/src/components/Navigation.vue
@@ -1,5 +1,12 @@
 <template>
   <nav>
-    This is the navigation.
+    <div class="logo_container">
+      <img src="@/assets/images/lettuce_logo_ph.png" class="logo" />
+    </div>
   </nav>
 </template>
+    
+<script>
+  export default {
+  }
+</script>
