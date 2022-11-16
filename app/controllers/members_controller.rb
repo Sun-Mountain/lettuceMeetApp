623af63e488aadffb8a41064978e47d5c3033d1d
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-11-16 09:19:58 -0500

    user controllers

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,19 @@
+class MembersController < ApplicationRecord
+  before_action :authenticate_user!
+
+  def show
+    user = get_user_from_token
+    render json: {
+      message: "If you see this, you're in!",
+      user: user
+    }
+  end
+
+  private
+
+  def get_user_from_token
+    jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], Rails.application.credentials.devise[:jwt_secret_key]).first
+    user_id = jwt_payload['sub']
+    user = User.find(user_id.to_s)
+  end
+end
