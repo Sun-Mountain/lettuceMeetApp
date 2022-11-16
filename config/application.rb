022d5683e47f260d9997f278fb3e92e222e86d57
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-11-16 10:25:24 -0500

    start over

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,6 @@
 require_relative "boot"
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-# require "active_job/railtie"
-require "active_record/railtie"
-# require "active_storage/engine"
-require "action_controller/railtie"
-# require "action_mailer/railtie"
-# require "action_mailbox/engine"
-# require "action_text/engine"
-require "action_view/railtie"
-# require "action_cable/engine"
-require "rails/test_unit/railtie"
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,14 +10,6 @@ module LettuceMeetApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
-    # Session store and session_otions
-    config.session_store :cookie_store, key: '_interslice_session'
-
-    # Required for all session management (reguardless of session_store)
-    config.middleware.use ActionDispatch::Cookies
-
-    config.middleware.use config.session_store, config.session_options
 
     # Configuration for the application, engines, and railties goes here.
     #
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users,
-             controllers: {
-               sessions: 'users/sessions',
-               registrations: 'users/registrations'
-             }
-
-  get '/member-data', to: 'memebers#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
