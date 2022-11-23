5ea733ee933486081469fcef68c69b5161d4e9a9
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-11-23 13:55:01 -0500

    add password verify

--- frontend/src/views/SignUpPage.vue
+++ frontend/src/views/SignUpPage.vue
@@ -3,13 +3,21 @@
     You're logged in!
   </div>
   <div v-else>
+    <CAlert color="danger"
+            class="d-flex align-items-center"
+            v-if="errors">
+      <CIcon icon="cil-warning" class="flex-shrink-0 me-2" width="24" height="24" />
+      <div>
+        {{ errors }}
+      </div>
+    </CAlert>
     <h3>Sign up!</h3>
     <CForm @submit="onSignUp">
       <div>
+        <CFormLabel for="inputEmail">Email</CFormLabel>
         <CFormInput
           type="email"
-          id="exampleFormControlInput1"
-          label="Email"
+          id="inputEmail"
           placeholder="name@example.com"
           aria-describedby="exampleFormControlInputHelpInline"
           v-model="signUpEmail"
@@ -18,6 +26,10 @@
       <div>
         <CFormLabel for="inputPassword2">Password</CFormLabel>
         <CFormInput type="password" id="inputPassword2" placeholder="Password" v-model="signUpPassword" />
+      </div>
+      <div>
+        <CFormLabel for="inputPassword3">Verify Password</CFormLabel>
+        <CFormInput type="password" id="inputPassword3" placeholder="Verify Password" v-model="passwordVerify" />
       </div>
       <div class="button-container">
         <CButton
@@ -41,21 +53,32 @@
     data() {
         return {
             signUpEmail: "",
-            signUpPassword: ""
+            signUpPassword: "",
+            passwordVerify: "",
+            errors: "",
         };
     },
     methods: {
         ...mapActions(["registerUser"]),
         onSignUp(event) {
-            event.preventDefault();
-            let data = {
-                user: {
-                    email: this.signUpEmail,
-                    password: this.signUpPassword,
-                },
-            };
-            this.registerUser(data);
-            this.resetData();
+        // Reset the alert to empty
+        this.errors = "";
+        // Verify that the passwords match
+        if (this.signUpPassword !== this.passwordVerify) {
+          this.errors = "Passwords must match";
+          return;
+        }
+          event.preventDefault();
+          let data = {
+              user: {
+                  email: this.signUpEmail,
+                  password: this.signUpPassword,
+              },
+          };
+          this.registerUser(data).catch(err => {
+            this.errors = err.message
+          });
+          this.resetData();
         },
         resetData() {
             this.signUpEmail = "";
