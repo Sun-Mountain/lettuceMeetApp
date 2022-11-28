b7dccdc5c81601456e1e312c180ba9dd26c8adf1
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-11-28 16:47:41 -0500

    navigation link styling

--- frontend/src/components/nav-item.vue
+++ frontend/src/components/nav-item.vue
@@ -9,15 +9,24 @@ const authStore = useAuthStore();
     <div class="nav_logo_container">
       <img src="@/assets/images/lettuce_nav_logo.png" class="logo" />
     </div>
-    <div v-show="authStore.user" class="navbar-nav">
+    <div class="nav_links_container">
       <router-link to="/" class="nav-item nav-link">Home</router-link>
-      <router-link to="/users" class="nav-item nav-link">Users</router-link>
+      <router-link v-show="authStore.user" to="/users" class="nav-item nav-link"
+        >Users</router-link
+      >
       <button
         @click="authStore.logout()"
+        v-show="authStore.user"
         class="btn btn-link nav-item nav-link"
       >
         Logout
       </button>
+      <router-link
+        v-show="!authStore.user"
+        to="register"
+        class="nav-item nav-link"
+        >Register</router-link
+      >
     </div>
   </nav>
 </template>
