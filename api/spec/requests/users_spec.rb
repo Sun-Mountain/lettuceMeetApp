7b58170c5e98b680b3fc4b68b3299e7d01d98e28
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-12-23 17:55:16 -0500

    random rspec

--- api/spec/requests/users_spec.rb
+++ api/spec/requests/users_spec.rb
@@ -4,8 +4,9 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'POST /users' do
+    let(:register_url) { '/users' }
+
     email = 'picard@gmail.com'
-    let(:url) { '/users' }
     let(:valid_user_params) do
       {
         first_name: 'Jean Luc',
@@ -14,6 +15,15 @@ RSpec.describe 'Users', type: :request do
         email: email,
         password: 'password',
         password_confirmation: 'password'
+      }
+    end
+
+    let(:login_url) { '/auth/login' }
+    let!(:user) { create :user }
+    let(:valid_sign_in) do
+      {
+        email: user.email,
+        password: user.password
       }
     end
 
@@ -30,7 +40,7 @@ RSpec.describe 'Users', type: :request do
 
     context 'registration is successful' do
       before do
-        post url, params: valid_user_params
+        post register_url, params: valid_user_params
       end
 
       it 'returns 201' do
@@ -43,8 +53,42 @@ RSpec.describe 'Users', type: :request do
       end
     end
 
-    # context 'login is successful' do
-    #   let(:user)
-    # end
+    context 'login is successful' do
+      before do
+        post login_url, params: valid_sign_in
+      end
+
+      it 'returns 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns last user' do
+        user_email = user.email
+        expect(response.body).to include(User.last.id.to_json)
+        expect(response.body).to include(user_email.to_json)
+      end
+
+      it 'returns with valid JWT' do
+        token = JSON.parse(response.body)['token']
+        expect { JwtToken.decode(token) }.to_not raise_error
+      end
+    end
+
+    context 'registration is unsuccessful' do
+      it 'returns 503' do
+        post register_url, params: invalid_user_params
+        expect(response).to have_http_status(503)
+      end
+    end
+
+    context 'login is unsuccessful' do
+      it 'returns 401' do
+        post login_url, params: {
+                                  email: user.email,
+                                  password: ''
+                                }
+        expect(response).to have_http_status(401)
+      end
+    end
   end
 end
