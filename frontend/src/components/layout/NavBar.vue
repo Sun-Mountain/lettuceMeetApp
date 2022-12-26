61e8876af6930ad1f761c6899e8f1a9cbc717069
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-12-26 11:02:42 -0500

    mobile nav view

--- frontend/src/components/layout/NavBar.vue
+++ frontend/src/components/layout/NavBar.vue
@@ -1,7 +1,14 @@
 <script setup>
+import { CAccordion, CAccordionHeader, CAccordionBody } from "@coreui/vue";
+import { ref } from "vue";
 import { useAuthStore } from "@/stores";
 
 const authStore = useAuthStore();
+const mobileDropDown = ref(false);
+
+function shiftMobileNav() {
+  mobileDropDown.value = !mobileDropDown.value;
+}
 </script>
 
 <template>
@@ -9,7 +16,53 @@ const authStore = useAuthStore();
     <div class="nav_logo_container">
       <img src="@/assets/images/lettuce_nav_logo.png" class="logo" />
     </div>
+
     <div class="nav_links_container">
+      <CAccordion flush class="nav_accordion">
+        <CAccordionItem>
+          <CAccordionHeader @click="shiftMobileNav" class="mobile_nav_header">
+            <font-awesome-icon
+              class="nav-direction"
+              icon="fa-solid fa-angles-up"
+              v-show="mobileDropDown"
+            />
+            <font-awesome-icon
+              class="nav-direction"
+              icon="fa-solid fa-angles-down"
+              v-show="!mobileDropDown"
+            />
+          </CAccordionHeader>
+          <CAccordionBody>
+            <div class="nav-links">
+              <router-link to="/" class="nav-item nav-link">Home</router-link>
+              <router-link
+                id="profile"
+                to="profile"
+                v-show="authStore.user"
+                class="nav-item nav-link"
+                >Profile</router-link
+              >
+              <button
+                id="logout"
+                @click="authStore.logout()"
+                v-show="authStore.user"
+                class="btn btn-link nav-item nav-link"
+              >
+                Logout
+              </button>
+              <router-link
+                id="register"
+                v-show="!authStore.user"
+                to="register"
+                class="nav-item nav-link"
+                >Register</router-link
+              >
+            </div>
+          </CAccordionBody>
+        </CAccordionItem>
+      </CAccordion>
+    </div>
+    <!-- <div class="nav_links_container">
       <router-link to="/" class="nav-item nav-link">Home</router-link>
       <router-link
         id="profile"
@@ -33,6 +86,6 @@ const authStore = useAuthStore();
         class="nav-item nav-link"
         >Register</router-link
       >
-    </div>
+    </div> -->
   </nav>
 </template>
