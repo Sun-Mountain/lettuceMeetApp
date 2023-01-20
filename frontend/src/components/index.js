0f1ecc316733a9c2eb877f0a3ba948ba09f54070
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-01-20 18:39:55 -0500

    add docker ignore file

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,6 +1,6 @@
 export { default as Alert } from "./AlertItem.vue";
 export { default as Nav } from "./NavBar.vue";
 
-export { default as DeskNav } from "../DeskNavLinks.vue";
+export { default as DeskNav } from "./DeskNavLinks.vue";
 
 export { default as MobileNav } from "./MobileNavLinks.vue";
