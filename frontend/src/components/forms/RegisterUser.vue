ad5f607ccc8e753f0175415446e567ba2cbf1dec
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-03-10 14:29:19 -0500

    register user function

--- frontend/src/components/forms/RegisterUser.vue
+++ frontend/src/components/forms/RegisterUser.vue
@@ -18,6 +18,7 @@ const schema = Yup.object().shape({
 async function onSubmit(values) {
   const usersStore = useUsersStore();
   const alertStore = useAlertStore();
+  console.log(values);
   try {
     await usersStore.register(values);
     await router.push('/account/login');
@@ -36,7 +37,7 @@ async function onSubmit(values) {
     variant="outlined"
   >
     <v-card-item>
-      <Form @submit="onSubmit" :validation-schema="schema" v-slot="{ errors, isSubmitting }">
+      <Form @submit="onSubmit" :validation-schema="schema" v-slot="{ errors, isSubmitting, isValid }">
         <div>
           <label>First Name:</label><br />
           <Field name="firstName" type="text" class="form-control" :class="{ 'is-invalid': errors.firstName }" />
--- frontend/src/stores/users.store.js
+++ frontend/src/stores/users.store.js
@@ -11,7 +11,15 @@ export const useUsersStore = defineStore({
   }),
   actions: {
     async register(user) {
-      await fetchWrapper.post(`${baseUrl}/users`, user)
+      let newUser = {
+        first_name: user.firstName,
+        last_name: user.lastName,
+        user_name: user.username,
+        email: user.email,
+        password: user.password,
+        password_confirmation: user.passwordConfirm
+      }
+      await fetchWrapper.post(`${baseUrl}users`, newUser)
     }
   }
 })
