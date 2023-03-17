6c0a0dbe169158873970a371939e007b51e2167f
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-03-17 10:42:27 -0400

    event store

---
+++ client/src/stores/event.store.js
@@ -0,0 +1,43 @@
+import { defineStore } from "pinia";
+
+import { fetchWrapper } from "@/helpers";
+import { useAlertStore, useAuthStore } from "@/stores";
+
+const baseUrl = `${import.meta.env.VITE_API_URL}`;
+
+export const useEventStore = defineStore({
+  id: "events",
+  state: () => ({
+    events: JSON.parse(localStorage.getItem("events")),
+    stagedEvent: {}
+  }),
+  actions: {
+    async createEvent(params) {
+      const alertStore = useAlertStore();
+      const authStore = useAuthStore();
+      try {
+        const userId = authStore.user.id;
+        await fetchWrapper.post(`${baseUrl}/users/${userId}/events`, params);
+        this.getAllUserEvents();
+      } catch (err) {
+        alertStore.error(err);
+      }
+      return this.events;
+    },
+    async getAllUserEvents() {
+      const alertStore = useAlertStore();
+      const authStore = useAuthStore();
+      try {
+        const userId = authStore.user.id;
+        const events = await fetchWrapper.get(
+          `${baseUrl}/users/${userId}/events`
+        );
+        this.events = events;
+        localStorage.setItem("events", JSON.stringify(events));
+      } catch (err) {
+        alertStore.error(err);
+      }
+      return this.events;
+    }
+  }
+})
--- client/src/stores/index.js
+++ client/src/stores/index.js
@@ -1,3 +1,4 @@
 export * from "./alert.store";
 export * from "./auth.store";
-export * from "./users.store"
+export * from "./event.store";
+export * from "./users.store";
