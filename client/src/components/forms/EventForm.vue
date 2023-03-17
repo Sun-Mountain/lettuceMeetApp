d2627f8dfffa5c85697524a4d2b2608cb0456712
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-03-17 10:45:32 -0400

    set up event form

---
+++ client/src/components/forms/EventForm.vue
@@ -0,0 +1,11 @@
+<script setup>
+  // import { Form, Field } from 'vee-validate';
+</script>
+
+<template>
+  <v-card
+    class="form-container"
+    title="Create Event"
+    variant="outlined"
+  ></v-card>
+</template>
