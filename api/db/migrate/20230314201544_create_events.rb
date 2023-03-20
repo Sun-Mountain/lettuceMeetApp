bbf55212160fea43ca652bad6df62c57eb9ab95b
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-03-20 07:54:50 -0400

    subnav styling

--- api/db/migrate/20230314201544_create_events.rb
+++ api/db/migrate/20230314201544_create_events.rb
@@ -5,7 +5,7 @@ class CreateEvents < ActiveRecord::Migration[7.0]
       t.string :eventTitle,    null: false
       t.text :description
       t.boolean :private, default: false, null: false
-      t.boolean :allDay,  default: false, null: false
+      t.boolean :allDay,  default: true, null: false
       t.date :startDate,  null: false
       t.date :endDate
       t.time :startTime
