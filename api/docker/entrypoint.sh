f0c3fa8e7bb4d52e2e42dd744d22a42e8376aad4
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-04-03 16:59:48 -0400

    remove rspec from entrypoint

--- api/docker/entrypoint.sh
+++ api/docker/entrypoint.sh
@@ -6,8 +6,6 @@ rm -f /usr/src/app/tmp/pids/server.pid
 
 echo "bundle install..."
 bundle check || bundle install --jobs 4
-echo "rspec spec..."
-bundle exec rspec spec
 echo "create and migrate..."
 bundle exec rake db:create db:migrate
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
