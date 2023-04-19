2aed2e1f6e22619db63fc92a0341f5c3eec11d7c
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-04-19 13:19:35 -0400

    remove cypress

--- frontend/src/components/forms/LoginUser.cy.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-import LoginUser from './LoginUser.vue';
-
-describe('<LoginUser />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-vue
-    cy.mount(LoginUser)
-  })
-})
