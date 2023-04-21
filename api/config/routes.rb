bea603015e507c121fe2a7c76cb37ddb8e766886
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-04-21 16:32:12 -0400

    install devise

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :users do
