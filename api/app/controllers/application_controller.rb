5a617a0ad032e994def4f14ddfeb2d8d88901e8d
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-04-23 16:32:56 -0400

    streamline sanitizer

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -10,11 +10,7 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |user|
-      user.permit(
-        :first_name, :last_name, :preferred_username, :email, :password, :password_confirmation
-      )
-    end
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name, last_name, preferred_username, email, password, password_confirmation])
   end
 
   private
