4aa8ec38ec135494b9f437e619c0fc9106edb039
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-04-23 14:31:58 -0400

    erase

--- api/app/serializers/user_serializer.rb
+++
@@ -1,8 +0,0 @@
-class UserSerializer
-  include FastJsonapi::ObjectSerializer
-  attributes :id, :email, :first_name, :last_name, :preferred_username, :created_at
-
-  attribute :created_date do |user|
-    user.created_at && user.created_at.strftime('%d/%m/%Y')
-  end
-end
