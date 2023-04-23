f8120913de8a5f1082eeccdd7f694bfb6aafc5a9
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-04-23 15:21:11 -0400

    install devise and devise user

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -9,10 +9,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-
-    resource '*',
-             headers: :any,
-             methods: %i[get post put patch delete options head]
+    origins 'http://localhost:3000'
+    resource(
+      '*',
+      headers: :any,
+      expose: ['access-token', 'expiry', 'token-type', 'Authorization'],
+      methods: [:get, :patch, :put, :delete, :post, :options, :show]
+    )
   end
 end
