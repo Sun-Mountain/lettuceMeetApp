692ab4b0ea4bd68150f08c69844037a7044c49c7
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-05-03 15:30:53 -0400

    add nav routes

---
+++ frontend/src/assets/json/index.ts
@@ -0,0 +1,1 @@
+export { default as navLinks } from './navigation-links.json';
--- frontend/src/components/MainNav.vue
+++ frontend/src/components/MainNav.vue
@@ -1,8 +1,26 @@
 <template>
-  <v-app-bar id="main-navigation">
-    Nav
-  </v-app-bar>
+  <nav>
+    <v-app-bar
+      :elevation="0"
+      id="main-navigation"
+      style="position:relative"
+    >
+      <div class="nav_logo_container">
+        <router-link to="/">
+          <img src="@/assets/images/lettuce_nav_logo.png" class="logo" />
+        </router-link>
+      </div>
+      <v-spacer />
+      <div class="nav_links_container">
+
+        <router-link v-for="link in navLinks" v-bind:key="link.name" v-bind:to="link.path">
+          {{ link.name }}
+        </router-link>
+      </div>
+    </v-app-bar>
+  </nav>
 </template>
 
 <script setup lang="ts">
+  import { navLinks } from '@/assets/json';
 </script>
