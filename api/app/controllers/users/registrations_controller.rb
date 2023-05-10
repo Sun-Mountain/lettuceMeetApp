4fe5fd9e3450a73ce57f144db0ff71cd09352dee
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-05-10 13:28:01 -0400

    refactor

--- api/app/controllers/users/registrations_controller.rb
+++ api/app/controllers/users/registrations_controller.rb
@@ -6,32 +6,43 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   private
 
+  def account_params
+    devise_parameter_sanitizer.sanitize(:account_update)
+  end
+
+  def register_user(resource)
+    render json: {
+      status: {code: 201, message: "Signed up sucessfully."},
+      data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+    }, status: :ok
+  end
+
   def respond_with(resource, _opts = {})
     if request.method == "POST" && resource.persisted?
-      render json: {
-        status: {code: 201, message: "Signed up sucessfully."},
-        data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
-      }, status: :ok
+      register_user(resource)
     elsif request.method == "PUT" && resource.persisted?
-      account_update_params = devise_parameter_sanitizer.sanitize(:account_update)
-      if authenticate_password(account_update_params) && @user&.update(account_update_params.except("current_password"))
-        render json: {
-          status: {code: 200, message: "Account updated successfully."},
-          data: UserSerializer.new(@user).serializable_hash[:data][:attributes]
-        }, status: :ok
-      elsif !authenticate_password
-        render json: { err: @user.errors.full_messages }, status: 401
-      else
-        render json: { err: @user.errors.full_messages }, status: 503
-      end
+      update_user(resource)
     elsif request.method == "DELETE"
       render json: {
         status: { code: 204, message: "Account deleted successfully."}
       }, status: :ok
     else
       render json: {
-        status: {code: 422, message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}"}
+        status: {code: 422, message: "Could not complete action. #{resource.errors.full_messages.to_sentence}"}
       }, status: 503
+    end
+  end
+
+  def update_user(resource)
+    if authenticate_password(account_params) && @user&.update(account_params.except("current_password"))
+      render json: {
+        status: {code: 200, message: "Account updated successfully."},
+        data: UserSerializer.new(@user).serializable_hash[:data][:attributes]
+      }, status: :ok
+    elsif !authenticate_password
+      render json: { err: @user.errors.full_messages }, status: 401
+    else
+      render json: { err: @user.errors.full_messages }, status: 503
     end
   end
 end
