d4b39cccf58f924d733076a7518adeefe2182175
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-05-16 14:03:18 -0400

    add event card

--- frontend/src/components/events/EventCard.vue
+++ frontend/src/components/events/EventCard.vue
@@ -1,8 +1,18 @@
 <template>
   <v-card>
-    Event Card
+    <h2>{{ event?.event_title }}</h2>
+    <div>
+      {{ event?.start_date }}
+    </div>
+
+    {{ event }}
   </v-card>
 </template>
 
 <script lang="ts" setup>
+import { toRefs } from 'vue';
+
+const props = defineProps({ event: Object })
+
+const { event } = toRefs(props);
 </script>
--- frontend/src/views/events/UserEvents.vue
+++ frontend/src/views/events/UserEvents.vue
@@ -2,12 +2,18 @@
   <v-container>
     <h1 v-if="events === 0 || events > 1">You have {{ events.length }} upcoming events.</h1>
     <h1 v-else>You have {{ events.length }} upcoming event.</h1>
+
+    <div>
+      <EventCard v-for="event in events" :key="event.uid" :event="event" />
+    </div>
   </v-container>
 </template>
 
 <script lang="ts" setup>
 import { storeToRefs } from 'pinia';
 import { useEventStore } from '@/store';
+
+import { EventCard } from '@/components';
 
 const eventStore = useEventStore();
 
