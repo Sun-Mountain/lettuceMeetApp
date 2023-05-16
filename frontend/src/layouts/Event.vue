e8129cdf4517e7dc5a8970027161fd7f7dabb185
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-05-16 14:11:55 -0400

    subnav layout

---
+++ frontend/src/layouts/Event.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container class="layout-with-subnav main-content-container">
+    <div>
+      SubNav
+    </div>
+    <div class="sub-content-container">
+      <router-view />
+    </div>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+</script>
--- frontend/src/router/events.routes.ts
+++ frontend/src/router/events.routes.ts
@@ -1,9 +1,9 @@
-import { View } from "@/layouts";
+import { EventLayout } from "@/layouts";
 import { UserEvents } from "@/views";
 
 export default {
   path: "/events",
-  component: View,
+  component: EventLayout,
   children: [
     { path: "", name: "upcoming", component: UserEvents }
   ]
