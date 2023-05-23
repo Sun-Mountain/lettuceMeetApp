da189102c92bb1c6ae596eb9c6e1812af0ef30db
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-05-23 10:16:57 -0400

    add user admin role

---
+++ frontend/src/helpers/user-roles.ts
@@ -0,0 +1,7 @@
+export const userAdmin = (user) => {
+  return user.admin;
+}
+
+export const userFullName = (firstName, lastName) => {
+  return firstName + " " + lastName;
+}
--- frontend/src/views/account/Profile.vue
+++ frontend/src/views/account/Profile.vue
@@ -1,10 +1,19 @@
 <template>
-  Profile
+  <div>
+    <h2>Welcome, {{ fullname }}</h2>
 
-  <router-link :to="{ name: 'accountEdit' }">
-    Edit
-  </router-link>
+    Profile
+  
+    <router-link :to="{ name: 'accountEdit' }">
+      Edit
+    </router-link>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import { useAuthStore } from '@/store';
+const { currentUser } = useAuthStore();
+import { userFullName } from '@/helpers';
+
+const fullname = userFullName(currentUser.first_name, currentUser.last_name);
 </script>
