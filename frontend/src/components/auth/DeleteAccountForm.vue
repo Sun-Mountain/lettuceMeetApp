c5047908117c6eeb39844d2b765044debf06499e
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-05-25 16:11:02 -0400

    delete account form component

---
+++ frontend/src/components/auth/DeleteAccountForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card
+    class="form-container"
+    title="Delete Account"
+    variant="outlined"
+  >
+    <v-card-item>
+      <Form>
+        <label>Current Password:</label>
+        <Field
+          name="current_password"
+          type="password"
+          class="form-control"
+        />
+      </Form>
+    </v-card-item>
+  </v-card>
+</template>
+
+<script lang="ts" setup>
+import { Form, Field } from 'vee-validate';
+</script>
