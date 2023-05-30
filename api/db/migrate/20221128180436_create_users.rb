58420e7e0919c016328eced8a6c3721c0623e625
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-05-30 13:41:33 -0400

    update user model tests

--- api/db/migrate/20221128180436_create_users.rb
+++ api/db/migrate/20221128180436_create_users.rb
@@ -3,10 +3,10 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :first_name,          null: false, default: ''
-      t.string :last_name,           null: false, default: ''
-      t.string :preferred_username,  null: false, default: ''
-      t.string :email,              null: false, default: ''
+      t.string :first_name,          null: false
+      t.string :last_name,           null: false
+      t.string :preferred_username,  null: false
+      t.string :email,               null: false
 
       t.timestamps
     end
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -31,10 +31,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_23_140310) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name", default: "", null: false
-    t.string "last_name", default: "", null: false
-    t.string "preferred_username", default: "", null: false
-    t.string "email", default: "", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "preferred_username", null: false
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "encrypted_password", default: "", null: false
