9c2af04f719052599694b0bdd599d5718aef042c
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-05-30 14:25:39 -0400

    committing

--- frontend/src/store/users.store.ts
+++ frontend/src/store/users.store.ts
@@ -17,7 +17,7 @@ export const useUsersStore = defineStore({
     },
     async register(user: User) {
       const newAccount = { user };
-      await fetchWrapper.post(`${BASE_URL}signup`, newAccount);
+      const res = await fetchWrapper.post(`${BASE_URL}signup`, newAccount);
     },
     async updateAccount(user: User, userId: number) {
       const authStore = useAuthStore();
