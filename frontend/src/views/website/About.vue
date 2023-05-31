c7de06707a14c57fe21f5828f8f1c79bb9d298c4
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-05-31 17:02:39 -0400

    update about page

--- frontend/src/views/website/About.vue
+++ frontend/src/views/website/About.vue
@@ -2,9 +2,24 @@
   <v-container>
     <v-container>
       <h1>Welcome to the Lettuce Meet App beta!</h1>
-      <p>
-        Lettuce Meet App started as a joke in 2019 at a coding bootcamp and evolved into a passion project. This application is currently in beta (otherwise known as: not its final form).
-      </p>
+        <p>
+          Lettuce Meet App started as a joke in 2019 at a coding bootcamp and evolved into a passion project. This application is currently in beta (otherwise known as: not its final form).
+        </p>
+      <h2 class="construction-header">
+        <v-icon color="orange-darken-2" icon="mdi:mdi-progress-wrench" />
+        Under Construction
+        <v-icon color="orange-darken-2" icon="mdi:mdi-progress-wrench" />
+      </h2>
+        <p>
+          This site is currently in the progress of being built. This may mean users may experience outages in some features.
+        </p>
+      <h2>Our Goal:</h2>
+        <p>
+          To make organizing events--private or public, personal or professional--easy.
+        </p>
+        <p>
+          Community is how we thrive, but nothing is more painful than trying to wrangle human beings.
+        </p>
     </v-container>
     <BackToHome />
   </v-container>
