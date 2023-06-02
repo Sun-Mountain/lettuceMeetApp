31147433df4e19525a8154c3f4e62ab2a31f6c8f
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-06-02 13:59:47 -0400

    install vue mq

--- frontend/src/plugins/index.ts
+++ frontend/src/plugins/index.ts
@@ -10,6 +10,7 @@ import vuetify from './vuetify'
 import { createPinia } from 'pinia'
 import router from '@/router'
 import VueDatePicker from '@vuepic/vue-datepicker';
+import VueMq from 'vue-mq'
 import '@vuepic/vue-datepicker/dist/main.css'
 
 // Types
@@ -19,6 +20,14 @@ export function registerPlugins (app: App) {
   loadFonts()
   app
     .component('VueDatePicker', VueDatePicker)
+    .use(VueMq, {
+      breakpoints: {
+        mobile: 450,
+        tablet: 900,
+        laptop: 1250,
+        desktop: Infinity,
+      }
+    })
     .use(vuetify)
     .use(router)
     .use(createPinia())
